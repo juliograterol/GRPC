5226e7ef61865235d0a7988014c8fc611088e570
Julio Graterol <juliograterolb@gmail.com>
2023-11-30 12:36:14 -0400

    first commit

---
+++ src/main/java/com/example/grpc_uru/GrpcUruApplication.java
@@ -0,0 +1,13 @@
+package com.example.grpc_uru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcUruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcUruApplication.class, args);
+	}
+
+}
